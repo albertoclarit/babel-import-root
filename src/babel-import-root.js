3d25010308b44120659265a0530abbbae48f1a9d
albert.oclarit <bbjao143213620>
2015-11-07 12:11:11 +0800

    Updates Prebuilt

---
+++ src/babel-import-root.js
@@ -0,0 +1,55 @@
+/**
+ * Created by albertoclarit on 11/7/15.
+ */
+export default function create({types:t}) {
+    class BabelImportRoot {
+
+        constructor(){
+            this.cwd = process.cwd();
+
+            return {
+                visitor: {
+                    ImportDeclaration: (node)=> {
+                        const givenPath = node.node.source.value;
+
+                        if (hasTildeInString(givenPath)) {
+                            node.node.source.value = transformRelativeToRootPath(node.node.source.value);
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+         transformRelativeToRootPath(path) {
+            if (hasTildeInString(path)) {
+                const withoutTilde = path.substring(2, path.length);
+                return  this.cwd + '/'+ withoutTilde;
+            }
+            if (typeof path === 'string') {
+                return path;
+            }
+            throw new Error('ERROR: No path passed');
+        }
+
+          hasTildeInString (path)  {
+
+            var containsTilde = false;
+
+            if (typeof path === 'string') {
+                const firstTwoCharactersOfString = path.substring(0, 2);
+                if (firstTwoCharactersOfString === '~/') {
+                    containsTilde = true;
+                }
+            }
+
+            return containsTilde;
+
+        }
+
+
+    }
+
+    return new BabelImportRoot();
+}
