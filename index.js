57e9e144eb782f614eb5e49e04d89ac998e482bd
albert.oclarit <bbjao143213620>
2015-11-07 13:31:19 +0800

    Updates Prebuilt

--- index.js
+++ index.js
@@ -1,201 +1,55 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
+/**
+ * Created by albertoclarit on 11/7/15.
+ */
 
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
+global.rootPath = process.cwd();
 
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
+function transformRelativeToRootPath(path) {
+    if (hasTildeInString(path)) {
+        const withoutTilde = path.substring(2, path.length);
+        return global.rootPath + '/'+ withoutTilde;
+    }
+    if (typeof path === 'string') {
+        return path;
+    }
+    throw new Error('ERROR: No path passed');
+}
 
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
+function  hasTildeInString (path)  {
 
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+    var containsTilde = false;
 
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+    if (typeof path === 'string') {
+        const firstTwoCharactersOfString = path.substring(0, 2);
+        if (firstTwoCharactersOfString === '~/') {
+            containsTilde = true;
+        }
+    }
 
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+    return containsTilde;
 
+}
 
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
+module.exports = function ( types) {
 
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
+    return {
+        visitor: {
+            ImportDeclaration: function(node) {
 
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+                //console.log(node.path);
 
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+                //var keys = Object.keys(node.node.source);
 
-	/* WEBPACK VAR INJECTION */(function(process, global) {'use strict';
+                //console.log(node.node.source.value);
+                const givenPath = node.node.source.value;
 
-	/**
-	 * Created by albertoclarit on 11/7/15.
-	 */
+                // node.node.source.value = 'xxx';
+                if (hasTildeInString(givenPath)) {
+                    node.node.source.value = transformRelativeToRootPath(node.node.source.value);
+                }
 
-	global.rootPath = process.cwd();
+            }
+        }
+    };
 
-	function transformRelativeToRootPath(path) {
-	    if (hasTildeInString(path)) {
-	        var withoutTilde = path.substring(2, path.length);
-	        return global.rootPath + '/' + withoutTilde;
-	    }
-	    if (typeof path === 'string') {
-	        return path;
-	    }
-	    throw new Error('ERROR: No path passed');
-	}
-
-	function hasTildeInString(path) {
-
-	    var containsTilde = false;
-
-	    if (typeof path === 'string') {
-	        var firstTwoCharactersOfString = path.substring(0, 2);
-	        if (firstTwoCharactersOfString === '~/') {
-	            containsTilde = true;
-	        }
-	    }
-
-	    return containsTilde;
-	}
-
-	module.exports = function (types) {
-
-	    return {
-	        visitor: {
-	            ImportDeclaration: function ImportDeclaration(node) {
-
-	                //console.log(node.path);
-
-	                //var keys = Object.keys(node.node.source);
-
-	                //console.log(node.node.source.value);
-	                var givenPath = node.node.source.value;
-
-	                // node.node.source.value = 'xxx';
-	                if (hasTildeInString(givenPath)) {
-	                    node.node.source.value = transformRelativeToRootPath(node.node.source.value);
-	                }
-	            }
-	        }
-	    };
-	};
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1), (function() { return this; }())))
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	// shim for using process in browser
-
-	var process = module.exports = {};
-	var queue = [];
-	var draining = false;
-	var currentQueue;
-	var queueIndex = -1;
-
-	function cleanUpNextTick() {
-	    draining = false;
-	    if (currentQueue.length) {
-	        queue = currentQueue.concat(queue);
-	    } else {
-	        queueIndex = -1;
-	    }
-	    if (queue.length) {
-	        drainQueue();
-	    }
-	}
-
-	function drainQueue() {
-	    if (draining) {
-	        return;
-	    }
-	    var timeout = setTimeout(cleanUpNextTick);
-	    draining = true;
-
-	    var len = queue.length;
-	    while(len) {
-	        currentQueue = queue;
-	        queue = [];
-	        while (++queueIndex < len) {
-	            if (currentQueue) {
-	                currentQueue[queueIndex].run();
-	            }
-	        }
-	        queueIndex = -1;
-	        len = queue.length;
-	    }
-	    currentQueue = null;
-	    draining = false;
-	    clearTimeout(timeout);
-	}
-
-	process.nextTick = function (fun) {
-	    var args = new Array(arguments.length - 1);
-	    if (arguments.length > 1) {
-	        for (var i = 1; i < arguments.length; i++) {
-	            args[i - 1] = arguments[i];
-	        }
-	    }
-	    queue.push(new Item(fun, args));
-	    if (queue.length === 1 && !draining) {
-	        setTimeout(drainQueue, 0);
-	    }
-	};
-
-	// v8 likes predictible objects
-	function Item(fun, array) {
-	    this.fun = fun;
-	    this.array = array;
-	}
-	Item.prototype.run = function () {
-	    this.fun.apply(null, this.array);
-	};
-	process.title = 'browser';
-	process.browser = true;
-	process.env = {};
-	process.argv = [];
-	process.version = ''; // empty string to avoid regexp issues
-	process.versions = {};
-
-	function noop() {}
-
-	process.on = noop;
-	process.addListener = noop;
-	process.once = noop;
-	process.off = noop;
-	process.removeListener = noop;
-	process.removeAllListeners = noop;
-	process.emit = noop;
-
-	process.binding = function (name) {
-	    throw new Error('process.binding is not supported');
-	};
-
-	process.cwd = function () { return '/' };
-	process.chdir = function (dir) {
-	    throw new Error('process.chdir is not supported');
-	};
-	process.umask = function() { return 0; };
-
-
-/***/ }
-/******/ ]);
+};
